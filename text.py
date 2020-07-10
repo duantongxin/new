28b324bddd8232ed01b704761a20f8cc727ebb34
duantongxin <68044611+duantongxin@users.noreply.github.com>
2020-07-10 15:12:14 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,22 +1,22 @@
 line = ">"
 while True:
     try:
-        input_fell = input("您要编辑文件的路径>>")
-        fell = open(input_fell, "w+")
+        input_file = input("您要编辑文件的路径>>")
+        file_ = open(input_file, "w+")
     except :
         print("请检查您的路径和权限！：-）")
         break
-    new_nput = input("您要[编辑]文件还是[读取]>>")
+    new_input = input("您要[编辑]文件还是[读取]>>")
     try:
-        if new_nput == "编辑":
+        if new_input == "编辑":
             while True :
                 one_input = input(line)
                 if one_input == "退出编辑":
                     break
-                fell.writelines(one_input)
-                fell.write("\r")
-                fell.flush()
-        elif new_nput == "读取":
+                file_.writelines(one_input)
+                file_.write("\r")
+                file_.flush()
+        elif new_input == "读取":
             for f_read in fell.readlines():
                 print(f_read)
         else:
@@ -32,5 +32,5 @@ while True:
             pass
     except:
         print("只能输入(y/n)小写!：-）")
-        fell.close()
+        file_.close()
         break
